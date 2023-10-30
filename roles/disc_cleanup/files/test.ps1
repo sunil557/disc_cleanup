65c44190765d6dfe94faeb2d6eb7550bdbb71dcb
Sunil Varma <sunil.varma@kyndryl.com>
2023-10-30 15:05:02 +0530

    updated

--- roles/disc_cleanup/files/test.ps1
+++ roles/disc_cleanup/files/test.ps1
@@ -1,112 +1,112 @@
 #$Block = {
-    param (
-        [string]$Username,
-        [string]$Password,
-        [string]$Source,
-        [string]$Days,
-    	[string]$DriveName
-    ) 
+param (
+	[string]$Username,
+	[string]$Password,
+	[string]$Source,
+	[string]$Days,
+	[string]$DriveName
+) 
 
-	Write-Output ([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)
+Write-Output ([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)
 
-	#$Username = "windows"
-	#$Password = "Welcome@1Welcome@1"
-	#$Password = ConvertTo-SecureString -string "$Password" -AsPlainText -Force 
+#$Username = "windows"
+#$Password = "Welcome@1Welcome@1"
+#$Password = ConvertTo-SecureString -string "$Password" -AsPlainText -Force 
 
-	$SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
-	Write-Output ("SecurePassword: ${SecurePassword}")
-	
-	$Creds = New-Object System.Management.Automation.PSCredential ($Username, $SecurePassword)
+$SecurePassword = ConvertTo-SecureString -String $Password -AsPlainText -Force
+Write-Output ("SecurePassword: ${SecurePassword}")
+
+$Creds = New-Object System.Management.Automation.PSCredential ($Username, $SecurePassword)
 
 
-	#$Source = "\\win1\new_share"
-	#$Days = "0"
-	#$DriveName="E"
+#$Source = "\\win1\new_share"
+#$Days = "0"
+#$DriveName="E"
 
-	$DrivePath=$DriveName+":\"
+$DrivePath=$DriveName+":\"
 
-	Write-Output ("Username: ${Username}")
-	Write-Output ("After_stg_passwd: ${Password}")
-	Write-Output ("Credss: ${Creds}")
-	Write-Output ("Source: ${Source}")
-	Write-Output ("Days: ${Days}")
-	Write-Output ("DriveName: ${DriveName}")
+Write-Output ("Username: ${Username}")
+Write-Output ("After_stg_passwd: ${Password}")
+Write-Output ("Credss: ${Creds}")
+Write-Output ("Source: ${Source}")
+Write-Output ("Days: ${Days}")
+Write-Output ("DriveName: ${DriveName}")
 
-	If ( Test-Path -path $DrivePath)
+If ( Test-Path -path $DrivePath)
+{
+
+Remove-PSDrive -Name $DriveName
+
+}
+
+else
+{
+Write-Output "Not removed"
+}
+
+New-PSDrive -Name $DriveName -Root $Source  -PSProvider "FileSystem" -Credential $Creds
+
+
+#$Days=100
+#$SharePath='\\DERUSUSRFL003.ey.net\003RUU10030\N\NADD_Files\_Data_transfer'
+#cd $SharePath
+$Filenames=(dir $DrivePath | select * | Sort-Object Name | select-object Name).Name
+
+Write-Output ("file_names: ${Filenames}")
+Write-Output ("DrivePath: ${TruePath}")
+
+if (!(Test-Path "$DrivePath\logs"))
+{
+	New-Item -ItemType "Directory" -Path "$DrivePath\logs"
+}
+else
+{
+	Write-Output "$DrivePath\logs"
+}
+
+if (!(Test-Path "$DrivePath\logs\$(get-date -f yyyy-MM-dd).log"))
+{
+	New-Item -ItemType "file" -Path "$DrivePath\logs\$(get-date -f yyyy-MM-dd).log"
+}
+else
+{
+	Write-Output "$DrivePath\logs\$(get-date -f yyyy-MM-dd).log"
+}
+
+
+foreach ( $Filename in $Filenames)
+{
+$TruePath=$DrivePath+'\'+$Filename
+$Files=Get-ChildItem -Recurse -LiteralPath $TruePath | Where-Object { !$_.PSIsContainer -and  ($_.CreationTime -lt (get-date).adddays(-$Days)) -and ($_.LastWriteTime -lt (get-date).adddays(-$Days)) }
+
+if ($Files.count -gt 0)
+{
+
+	ForEach( $File in $Files)
 	{
-
-	Remove-PSDrive -Name $DriveName
-
+		Write-Output $File.FullName
+		Remove-Item -Path $File.FullName -Recurse -Force -Confirm:$false -ErrorAction SilentlyContinue
+		Add-Content -Path "$DrivePath\logs\$(get-date -f yyyy-MM-dd).log" -Value $File.FullName
 	}
+}
+}
 
-	else
-	{
-	Write-Output "Not removed"
-	}
+#Get-ChildItem -Path \\DERUSUSRFL003.ey.net\003RUU10030\N\NADD_Files\OT30D\_Data_transfer -Recurse | foreach { Write-Output $_.Name }
 
-	New-PSDrive -Name $DriveName -Root $Source  -PSProvider "FileSystem" -Credential $Creds
+#$FolderPath="\\DERUSUSRFL003.ey.net\003RUU10030\N\NADD_Files\OT30D\_Data_transfer\AV Center Rental"
+#Test-Path -Path $FolderPath
 
+If ( Test-Path -path $DrivePath)
+{
 
-	#$Days=100
-	#$SharePath='\\DERUSUSRFL003.ey.net\003RUU10030\N\NADD_Files\_Data_transfer'
-	#cd $SharePath
-	$Filenames=(dir $DrivePath | select * | Sort-Object Name | select-object Name).Name
+Remove-PSDrive -Name $DriveName
 
-	Write-Output ("file_names: ${Filenames}")
-	Write-Output ("DrivePath: ${TruePath}")
+}
 
-	if (!(Test-Path "$DrivePath\logs"))
-	{
-		New-Item -ItemType "Directory" -Path "$DrivePath\logs"
-	}
-	else
-	{
-		Write-Output "$DrivePath\logs"
-	}
-
-	if (!(Test-Path "$DrivePath\logs\$(get-date -f yyyy-MM-dd).log"))
-	{
-		New-Item -ItemType "file" -Path "$DrivePath\logs\$(get-date -f yyyy-MM-dd).log"
-	}
-	else
-	{
-		Write-Output "$DrivePath\logs\$(get-date -f yyyy-MM-dd).log"
-	}
-
-
-	foreach ( $Filename in $Filenames)
-	{
-	$TruePath=$DrivePath+'\'+$Filename
-	$Files=Get-ChildItem -Recurse -LiteralPath $TruePath | Where-Object { !$_.PSIsContainer -and  ($_.CreationTime -lt (get-date).adddays(-$Days)) -and ($_.LastWriteTime -lt (get-date).adddays(-$Days)) }
-
-	if ($Files.count -gt 0)
-	{
-
-		ForEach( $File in $Files)
-		{
-			Write-Output $File.FullName
-			Remove-Item -Path $File.FullName -Recurse -Force -Confirm:$false -ErrorAction SilentlyContinue
-			Add-Content -Path "$DrivePath\logs\$(get-date -f yyyy-MM-dd).log" -Value $File.FullName
-		}
-	}
-	}
-
-	#Get-ChildItem -Path \\DERUSUSRFL003.ey.net\003RUU10030\N\NADD_Files\OT30D\_Data_transfer -Recurse | foreach { Write-Output $_.Name }
-
-	#$FolderPath="\\DERUSUSRFL003.ey.net\003RUU10030\N\NADD_Files\OT30D\_Data_transfer\AV Center Rental"
-	#Test-Path -Path $FolderPath
-
-	If ( Test-Path -path $DrivePath)
-	{
-
-	Remove-PSDrive -Name $DriveName
-
-	}
-
-	else
-	{
-	Write-Output "Not removed"
-	}
+else
+{
+Write-Output "Not removed"
+}
 
 # }
 
